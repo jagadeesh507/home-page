4c1b67df5a739f18b91d468ac19a6a6593bd0165
jagadeeesh507 <jagadeeshm778@gmail.com>
2024-01-29 14:47:09 +0530

    updated: favicon changed

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,31 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
-            <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+  return (
+    <Html lang="en">
+      <Head />
+      <link
+        rel="shortcut icon"
+        href="/images/ContentQL.ico"
+        type="image/x-icon"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      />
 
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
